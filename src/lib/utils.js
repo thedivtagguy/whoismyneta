0f125351f347034d11024754b3e705effac8937a
thedivtagguy <amanbhargava2001@gmail.com>
2024-04-24 09:45:54 +0530

    fix: currency formatting

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -43,12 +43,18 @@ export function setCandidate(candidate = '') {
 }
 
 export function formatRupee(value) {
-	return new Intl.NumberFormat('en-IN', {
-		style: 'currency',
-		currency: 'INR',
-		minimumFractionDigits: 0,
-		maximumFractionDigits: 0
-	}).format(value);
+	if (value >= 10000000) {
+		return (value / 10000000).toFixed(2) + ' Cr';
+	} else if (value >= 100000) {
+		return (value / 100000).toFixed(2) + ' L';
+	} else {
+		return new Intl.NumberFormat('en-IN', {
+			style: 'currency',
+			currency: 'INR',
+			minimumFractionDigits: 0,
+			maximumFractionDigits: 0
+		}).format(value);
+	}
 }
 
 export function radiansToDegrees(radians) {
