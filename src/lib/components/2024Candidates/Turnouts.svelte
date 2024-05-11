802dca02c74f2ec24b82f56c4c6333f75e09902e
thedivtagguy <amanbhargava2001@gmail.com>
2024-05-11 20:47:52 +0530

    chore: progress

--- src/lib/components/2024Candidates/Turnouts.svelte
+++ src/lib/components/2024Candidates/Turnouts.svelte
@@ -1,6 +1,15 @@
 <script>
-	import { Chart, Svg, Spline, Axis, Text, Tooltip, TooltipItem, Highlight } from 'layerchart';
-
+	import {
+		Chart,
+		Svg,
+		Labels,
+		Spline,
+		Axis,
+		Text,
+		Tooltip,
+		TooltipItem,
+		Highlight
+	} from 'layerchart';
 	import { scaleTime } from 'd3';
 	import { format } from 'date-fns';
 
@@ -9,10 +18,6 @@
 	$: turnouts = Object.keys(historical).map((year) => {
 		return { date: new Date(year), value: historical[year].Turnout };
 	});
-
-	$: console.log(turnouts);
-
-	$: minTurnout = Math.min(...turnouts.map((d) => d.value));
 </script>
 
 <div class="flex flex-col gap-2 md:flex-col md:justify-start">
@@ -27,11 +32,7 @@
 			tooltip={{ mode: 'bisect-x' }}
 		>
 			<Svg>
-				{#each turnouts as { date, value }, i}
-					<Text x={date} y={value} dy={i % 2 === 0 ? 12 : -12} text-anchor="middle">
-						{value}%
-					</Text>
-				{/each}
+				<Labels format="decimal" />
 				<Axis placement="bottom" rule ticks={2} />
 				<Spline class="stroke-2 stroke-primary" />
 				<Highlight points lines />
