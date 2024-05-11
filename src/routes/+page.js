9752eb890280985311d9bcbf9fe25be34e77acf4
thedivtagguy <amanbhargava2001@gmail.com>
2024-05-12 02:06:07 +0530

    random constituncies test

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -24,7 +24,7 @@ export const load = async ({ fetch, url }) => {
 			} = await response.json();
 
 			const constituencies = data.filter(
-				(constituencyData) => slugify(constituencyData.ls_seat_name) === state
+				(constituencyData) => constituencyData.ls_seat_name === state
 			);
 
 			const randomConstituencies = constituencies.sort(() => 0.5 - Math.random()).slice(0, 5);
